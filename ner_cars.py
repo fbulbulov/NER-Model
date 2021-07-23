88df91bed0fc579e3741ed03120e3375633554b1
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-23 19:47:11 +0200

    cars lists

--- ner_cars.py
+++ ner_cars.py
@@ -10,3 +10,11 @@ with open("cars.txt") as file:
 nlp = spacy.load("en_core_web_lg")
 doc = nlp(dataset)
 print("Entities:", [(ent.text, ent.label_) for ent in doc.ents])
+
+words= [ "Abarth", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Bugatti", "Cadillac", "Chevrolet", "Chrysler", "Citroën",
+        "Dacia","Daewoo","Daihatsu","Dodge","Donkervoort","DS", "Ferrari", "Fiat", "Fisker", "Ford", "Honda", "Hummer", "Hyundai",
+        "Infiniti","Iveco","Jaguar","Jeep","Kia","KTM","Lada","Lamborghini","Lancia","Land Rover","Landwind","Lexus","Lotus","Maserati",
+        "Maybach","Mazda","McLaren","Mercedes-Benz","MG","Mini","Mitsubishi","Morgan","Nissan","Opel","Peugeot","Porsche","Renault",
+        "Rolls-Royce","Saab","Seat","Skoda","Smart","SsangYong","Subaru","Suzuki","Tesla","Toyota","Volkswagen","Volvo",]
+
+train_data = []
