926f266a92cf41e3794cb9900edbeff0febfca88
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-23 19:42:39 +0200

    loading entites

--- ner_cars.py
+++ ner_cars.py
@@ -4,5 +4,9 @@ from spacy.util import minibatch, compounding
 from pathlib import Path
 from spacy.training import Example
 
+with open("cars.txt") as file:
+    dataset = file.read()
 
-
+nlp = spacy.load("en_core_web_lg")
+doc = nlp(dataset)
+print("Entities:", [(ent.text, ent.label_) for ent in doc.ents])
